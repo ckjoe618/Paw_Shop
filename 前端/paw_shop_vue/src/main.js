cb59c30fb10c9f444856d22739e2348b4aefd6fa
william <michelle71022@gmail.com>
2025-04-14 17:55:37 +0800

    區分前後端

---
+++ 前端/paw_shop_vue/src/main.js
@@ -0,0 +1,21 @@
+// 建立 APP
+import { createApp } from "vue";
+import App from "./App.vue";
+const app = createApp(App);
+
+// 使用 router 插件
+import router from "./router";
+app.use(router);
+
+// 使用 pinia 插件
+import { createPinia } from "pinia";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate); // 使用持久化插件，能避免 F5 後，全部遺忘
+app.use(pinia);
+
+// 使用 Font Awesome 插件
+import "@fortawesome/fontawesome-free/css/all.css";
+
+// 掛載 APP
+app.mount("#app");
