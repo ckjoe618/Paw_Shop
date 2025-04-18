e209d60f32bbf3da07975330f1f3079bef08eb61
Andrew <andrew.linyc@gmail.com>
2025-04-18 15:18:41 +0800

    小修

--- 前端/paw_shop_vue/src/router/index.js
+++ 前端/paw_shop_vue/src/router/index.js
@@ -60,7 +60,7 @@ const routes = [
         component: () => import("@/member/pages/AdminMemberPage.vue"),
       },
       {
-        path: '/admin/products',
+        path: 'products',
         component: () => import('@/product/backsite/ProductListPage.vue')
       },
       {
@@ -81,29 +81,29 @@ const router = createRouter({
   routes,
 });
 
-// router.beforeEach((to, from, next) => {
-//   const authStore = useAuthStore();
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
 
-//   if (to.meta.requiresAuth && !authStore.isLoggedIn) {
-//     // 未登入卻想進入需要登入的頁面
-//     return next("/login");
-//   }
-
-//   if (to.meta.requiresGuest && authStore.isLoggedIn) {
-//     // 已登入卻想進登入頁，導回首頁
-//     return next("/");
-//   }
-
-if (to.meta.requiresAdmin) {
-  if (!authStore.isLoggedIn) {
+  if (to.meta.requiresAuth && !authStore.isLoggedIn) {
+    // 未登入卻想進入需要登入的頁面
     return next("/login");
   }
-  if (authStore.role !== "ADMIN") {
-    return next("/403"); // 沒權限
-  }
-}
 
-//   return next();
-// });
+  if (to.meta.requiresGuest && authStore.isLoggedIn) {
+    // 已登入卻想進登入頁，導回首頁
+    return next("/");
+  }
+
+  if (to.meta.requiresAdmin) {
+    if (!authStore.isLoggedIn) {
+      return next("/login");
+    }
+    if (authStore.role !== "ADMIN") {
+      return next("/403"); // 沒權限
+    }
+  }
+
+  return next();
+});
 
 export default router;
