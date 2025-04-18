d77ac448b4141e81a91a58919d9f38392c07326b
粘偉晨 <michelle71022@gmail.com>
2025-04-18 11:34:05 +0800

    修正 後臺預約 bug

--- 前端/paw_shop_vue/src/router/index.js
+++ 前端/paw_shop_vue/src/router/index.js
@@ -19,23 +19,26 @@ const routes = [
         path: "home",
         component: () => import("@/pages/FrontHome.vue"),
       },
-      // { path: "xx", component:  },
       {
-        path: 'toappointments',
-        component: () => import("@/appointment/components/AppointmentFrontPage.vue")
+        path: "toappointments",
+        component: () =>
+          import("@/appointment/components/AppointmentFrontPage.vue"),
       },
       {
-        path: 'toappointments/reserve',
-        component: () => import("@/appointment/components/AppointmentReserve.vue")
+        path: "toappointments/reserve",
+        component: () =>
+          import("@/appointment/components/AppointmentReserve.vue"),
       },
       {
-        path: 'toappointments/hendlereserve',
-        component: () => import("@/appointment/components/AppointmentHendlePage.vue")
+        path: "toappointments/hendlereserve",
+        component: () =>
+          import("@/appointment/components/AppointmentHendlePage.vue"),
       },
       {
-        path: 'toappointments/queryreserve',
-        component: () => import("@/appointment/components/AppointmentQueryPage.vue")
-      }
+        path: "toappointments/queryreserve",
+        component: () =>
+          import("@/appointment/components/AppointmentQueryPage.vue"),
+      },
     ],
   },
   {
@@ -60,14 +63,15 @@ const routes = [
         component: () => import("@/member/pages/AdminMemberPage.vue"),
       },
       {
-        path: 'appointments',
-        component: () => import("@/appointment/components/AppointmentList.vue")
+        path: "appointments",
+        component: () => import("@/appointment/components/AppointmentList.vue"),
       },
       {
-        path: '/appointments/edit/:id',
-        name: 'AppointmentEdit',
-        component: () => import('@/appointment/components/AppointmentEditPage.vue')
-      }
+        path: "/appointments/edit/:id",
+        name: "AppointmentEdit",
+        component: () =>
+          import("@/appointment/components/AppointmentEditPage.vue"),
+      },
     ],
   },
 ];
@@ -77,29 +81,29 @@ const router = createRouter({
   routes,
 });
 
-// router.beforeEach((to, from, next) => {
-//   const authStore = useAuthStore();
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
 
-//   if (to.meta.requiresAuth && !authStore.isLoggedIn) {
-//     // 未登入卻想進入需要登入的頁面
-//     return next("/login");
-//   }
-
-//   if (to.meta.requiresGuest && authStore.isLoggedIn) {
-//     // 已登入卻想進登入頁，導回首頁
-//     return next("/");
-//   }
-
-if (to.meta.requiresAdmin) {
-  if (!authStore.isLoggedIn) {
+  if (to.meta.requiresAuth && !authStore.isLoggedIn) {
+    // 未登入卻想進入需要登入的頁面
     return next("/login");
   }
-  if (authStore.role !== "ADMIN") {
-    return next("/403"); // 沒權限
-  }
-}
 
-//   return next();
-// });
+  if (to.meta.requiresGuest && authStore.isLoggedIn) {
+    // 已登入卻想進登入頁，導回首頁
+    return next("/");
+  }
+
+  if (to.meta.requiresAdmin) {
+    if (!authStore.isLoggedIn) {
+      return next("/login");
+    }
+    if (authStore.role !== "ADMIN") {
+      return next("/403"); // 沒權限
+    }
+  }
+
+  return next();
+});
 
 export default router;
